4f529b345dca4f82a218ad939829e0d0d62baf83
Om Jaiswal <jom15718@gmail.com>
2026-03-08 14:03:54 +0530

    feat: add Satoshi font files and update README for webfont installation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="bg-zinc-900 w-full h-screen text-white">
+    <div className="bg-zinc-900 w-full font-['Satoshi_variable'] h-screen text-white">
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function cla () {
+function cla() {
   return (
-    <div className="max-w-screen-xl bg-sky-100 p-3">
-        Om 
+    <div className="max-w-screen-xl mx-auto flex items-center  py-6 ml-10">
+      <img
+        src="https://cdn.prod.website-files.com/6552fa0e9db1071c9d279ba9/6654005f64b18d1eaa48cf60_Refokus.png"
+        alt="Refokus Logo"
+        className="h-20 w-auto"
+      />
+      <div className="links flex gap-14 ">
+        {["Home", "Work", "Culture"].map((elem,index) => (
+          <a href="#" className="text-white  flex items-center gap-1 text-sm">
+          {index=== 1 &&(<span style={{boxShadow:"0 0 0.45em #00FF19"}} className="inline-block rounded-full w-1 h-1 bg-green-500 "></span>) }  
+            {elem}
+          </a>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default cla 
+export default cla;
