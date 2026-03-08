8b640dfbe4557d9976d3cffe84978d5af2313c48
Om Jaiswal <jom15718@gmail.com>
2026-03-08 18:50:43 +0530

    feat: add Cards component and enhance Maruees component to render multiple Maruee instances with dynamic image URLs

--- src/components/Maruee.jsx
+++ src/components/Maruee.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-function Maruee() {
+function Maruee({ imagesurl }) {
   return (
-    <div>Maruee</div>
-  )
+    <div className="w-full overflow-hidden py-8">
+      <div className="flex items-center gap-16 whitespace-nowrap">
+        {imagesurl.map((item, index) => (
+          <img
+            key={index}
+            src={item}
+            alt="logo"
+            className="h-12 w-auto object-contain opacity-60 hover:opacity-100 transition duration-300 flex-shrink-0"
+          />
+        ))}
+        {imagesurl.map((item, index) => (
+          <img
+            key={index}
+            src={item}
+            alt="logo"
+            className="h-12 w-auto object-contain opacity-60 hover:opacity-100 transition duration-300 flex-shrink-0"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Maruee
+export default Maruee;
--- src/components/Maruees.jsx
+++ src/components/Maruees.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
-import Maruee from './Maruee';
+import React from "react";
+import Maruee from "./Maruee";
 
 function Maruees() {
+  const images = [
+    [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+
+      "https://cdn.simpleicons.org/google",
+      "https://cdn.simpleicons.org/apple",
+      "https://cdn.simpleicons.org/netflix",
+      "https://cdn.simpleicons.org/spotify",
+      "https://cdn.simpleicons.org/tesla",
+      "https://cdn.simpleicons.org/uber",
+      "https://cdn.simpleicons.org/airbnb",
+      "https://cdn.simpleicons.org/slack",
+      "https://cdn.simpleicons.org/paypal",
+      "https://cdn.simpleicons.org/amazon"
+    ],
+    [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+
+      "https://cdn.simpleicons.org/facebook",
+      "https://cdn.simpleicons.org/instagram",
+      "https://cdn.simpleicons.org/linkedin",
+      "https://cdn.simpleicons.org/youtube",
+      "https://cdn.simpleicons.org/twitter",
+      "https://cdn.simpleicons.org/discord",
+      "https://cdn.simpleicons.org/notion",
+      "https://cdn.simpleicons.org/dropbox",
+      "https://cdn.simpleicons.org/samsung",
+      "https://cdn.simpleicons.org/intel"
+    ]
+  ];
+
   return (
-    <div>
-        <Maruee />
+    <div className="w-full">
+      {images.map((item, index) => (
+        <Maruee key={index} imagesurl={item} />
+        
+      ))}
     </div>
-  )
+  );
 }
 
-export default Maruees
+export default Maruees;
